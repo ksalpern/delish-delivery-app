bd1566e35454f2d919ec726d8d6cb27e991fa451
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-05-28 21:31:53 +0300

    update success&error pages style

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Error.scss'
 
 const Error = () => {
   return (
-    <div>
-      Error
+    <div className='error'>
+      <h1>Oops! Something went wrong.</h1>
+      <p>
+        We apologize for the inconvenience, but it seems that an error occurred
+        while processing your order.
+      </p>
+      <p>
+        Please try again later or contact our customer support for assistance.
+      </p>
+      <p>Thank you for your understanding.</p>
+      <Link to='/'>
+        <button>Continue shopping</button>
+      </Link>
     </div>
   )
 }
--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Success.scss'
 
 const Success = () => {
   return (
-    <div>
-      Success
+    <div className='success'>
+      <h1>Form Submitted Successfully!</h1>
+      <p>
+        Thank you for placing your order. We have received your information and
+        will process your order shortly.
+      </p>
+      <p>We appreciate your order and look forward to serving you again!</p>
+      <Link to='/'>
+        <button>Continue shopping</button>
+      </Link>
     </div>
   )
 }
