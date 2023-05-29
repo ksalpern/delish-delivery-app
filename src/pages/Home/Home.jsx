750a411c2b005d885e6432fd52b1dc7574e7e0c5
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-05-29 17:46:11 +0300

    update adaptive style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import './Home.scss'
-import ShopCard from '../../components/ShopCard/ShopCard'
+import './Home.scss';
+import ShopCard from '../../components/ShopCard/ShopCard';
 
 const Home = () => {
-  const [shops, setShops] = useState([])
+  const [shops, setShops] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://6471f2e36a9370d5a41adc0b.mockapi.io/shops')
-      .then(res => setShops(res.data))
-  }, [])
+      .then((res) => setShops(res.data));
+  }, []);
 
   return (
-    <div className='home'>
-      {shops.map(shop => (
-        <ShopCard key={shop.id} {...shop} />
-      ))}
+    <div className="home">
+      <p>
+      Enjoy our wide palette of flavors and fill yourself with the tastiest food. Our menu includes a variety of cuisines, from traditional national dishes to exotic international dishes that will satisfy the taste of even the most demanding gourmets.
+      </p>
+      <div className="home__shops">
+        {shops.map((shop) => (
+          <ShopCard key={shop.id} {...shop} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -11,6 +11,7 @@ const Success = () => {
         Thank you for placing your order. We have received your information and
         will process your order shortly.
       </p>
+      <p>The product will be sent to you from the nearest store.</p>
       <p>We appreciate your order and look forward to serving you again!</p>
       <Link to='/'>
         <button>Continue shopping</button>
